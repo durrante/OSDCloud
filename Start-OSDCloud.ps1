fe64b64334d4dc37741f992d9f11a2f12ba27528
durrante <64601521+durrante@users.noreply.github.com>
2021-12-20 10:29:48 +0000

    Create Start-OSDCloud.ps1

---
+++ Start-OSDCloud.ps1
@@ -0,0 +1,19 @@
+Write-Host -ForegroundColor Green "Starting OSDCloud"
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+#Start OSDCloudScriptPad
+Write-Host -ForegroundColor Green "Start OSDPad"
+Start-OSDPad -RepoOwner Durrante -RepoName OSDCloud -RepoFolder OSDCloudDeploy -BrandingTitle 'OSDCloud Deployment'
