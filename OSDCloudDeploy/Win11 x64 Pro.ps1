879bcb98b93ec4719e3c9cb6b7b4f1e1a18ad2b5
durrante <64601521+durrante@users.noreply.github.com>
2024-02-18 15:30:47 +0000

    Update Win11 x64 Pro.ps1

--- OSDCloudDeploy/Win11 x64 Pro.ps1
+++ OSDCloudDeploy/Win11 x64 Pro.ps1
@@ -25,6 +25,7 @@ Install-Module OSD -Force -AllowClobber -SkipPublisherCheck
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force 
 
+#    Produce warning on data erase
 Write-Host "`n DISCLAIMER: - Going further will erase all data on your disk ! `n"-ForegroundColor Red
 
 #================================================
