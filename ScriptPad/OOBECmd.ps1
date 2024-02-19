c7460c116b7325c192ce8a9ea035b41eade7aa26
durrante <64601521+durrante@users.noreply.github.com>
2024-02-19 12:01:38 +0000

    Create OOBECmd.ps1

---
+++ ScriptPad/OOBECmd.ps1
@@ -0,0 +1,83 @@
+## This script triggers OOBEDeploy.cmd upon first boot.
+
+$scriptFolderPath = "$env:ProgramData\OSDeploy\Custom_Scripts\"
+$ScriptPathOOBE = $(Join-Path -Path $env:SystemDrive\Windows\ -ChildPath "OOBEDeploy.cmd")
+$ScriptPathSendKeys = $(Join-Path -Path $scriptFolderPath -ChildPath "SendKeys.ps1")
+
+If(!(Test-Path -Path $scriptFolderPath)) {
+    New-Item -Path $scriptFolderPath -ItemType Directory -Force | Out-Null
+}
+
+$SendKeysScript = @"
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SendKeys.log"
+Start-Transcript -Path (Join-Path "`$env:ProgramData\OSDeploy\Custom_Scripts\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
+
+Write-Host -ForegroundColor DarkGray "Stop Debug-Mode (SHIFT + F10) with WscriptShell.SendKeys"
+`$WscriptShell = New-Object -com Wscript.Shell
+
+# ALT + TAB
+Write-Host -ForegroundColor DarkGray "SendKeys: ALT + TAB"
+`$WscriptShell.SendKeys("%({TAB})")
+
+Start-Sleep -Seconds 1
+
+# Shift + F10
+Write-Host -ForegroundColor DarkGray "SendKeys: SHIFT + F10"
+`$WscriptShell.SendKeys("+({F10})")
+
+Stop-Transcript -Verbose | Out-File
+"@
+
+Out-File -FilePath $ScriptPathSendKeys -InputObject $SendKeysScript -Encoding ascii
+
+# Download ServiceUI.exe
+Write-Host -ForegroundColor Gray "Download ServiceUI.exe from GitHub Repo"
+Invoke-WebRequest https://github.com/durrante/OSDCloud/raw/main/ServiceUI64.exe -OutFile "C:\OSDCloud\ServiceUI.exe"
+
+#Create Scheduled Task for SendKeys with 15 seconds delay
+$TaskName = "Scheduled Task for SendKeys"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT15S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathSendKeys + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
+
+# Create Scheduled Task for OSDCloud post installation with 20 seconds delay
+$TaskName = "Scheduled Task for OSDCloud post installation"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT20S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathOOBE + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
