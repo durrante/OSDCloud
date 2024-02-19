8f79056f17e816b2e661e71f424c120c66288bd7
durrante <64601521+durrante@users.noreply.github.com>
2024-02-19 11:28:14 +0000

    Create Set-EmbeddedBIOSProductKey.ps1

---
+++ ScriptPad/Set-EmbeddedBIOSProductKey.ps1
@@ -0,0 +1,21 @@
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Install-EmbeddedProductKey.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\OSDeploy\Custom_Scripts\" $Global:Transcript) -ErrorAction Ignore
+
+Write-Host -ForegroundColor Green "Get embedded product key"
+$Key = (Get-WmiObject SoftwareLicensingService).OA3xOriginalProductKey
+
+If ($Key) {
+    Write-Host -ForegroundColor Green "Installing embedded product key"
+    Invoke-Command -ScriptBlock {& 'cscript.exe' "$env:windir\system32\slmgr.vbs" '/ipk' "$($Key)"}
+    Start-Sleep -Seconds 5
+
+    Write-Host -ForegroundColor Green "Activating embedded product key"
+    Invoke-Command -ScriptBlock {& 'cscript.exe' "$env:windir\system32\slmgr.vbs" '/ato'}
+    Start-Sleep -Seconds 5
+}
+
+Else {
+    Write-Host -ForegroundColor Red 'No embedded product key found.'
+}
+
+Stop-Transcript
